eec38f10a032993c4b6660b4c2b27b640bfd7530
Bello Ajibola <bellohargbola13@gmail.com>
2017-12-29 11:03:51 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="app">
+    <md-app class="md-scrollbar" md-waterfall md-mode="fixed">
+      <md-app-toolbar class="md-primary animated fadeInTop" md-elevation="0">
+        <md-button class="md-icon-button pointer" @click="toggleMenu" v-if="!menuVisible">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <h3 class="md-title pointer" style="flex: 1"><router-link to="/"><span class="logoColor">MedFind</span></router-link></h3>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible" md-persistent="full" md-elevation="5">
+        <md-toolbar class="md-transparent" md-elevation="0">
+          <span>Navigation</span>
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button md-dense" @click="toggleMenu">
+              <md-icon>keyboard_arrow_left</md-icon>
+            </md-button>
+          </div>
+        </md-toolbar>
+
+        <md-list>
+          <router-link to="/">
+            <md-list-item @click="mobileMenu" class="pointer">
+              <md-icon>home</md-icon>
+              <span class="md-list-item-text">Home</span>
+            </md-list-item>
+          </router-link>
+
+          <router-link to="/UserGuide">
+           <md-list-item @click="mobileMenu" class="pointer">
+              <md-icon>help_outline</md-icon>
+              <span class="md-list-item-text">User Guide</span>
+            </md-list-item>
+          </router-link>
+        </md-list>
+      </md-app-drawer>
+      <md-app-content class="appContent">
+        <router-view></router-view>
+         <md-speed-dial class="md-bottom-right" md-direction="top" md-event="click">
+            <md-speed-dial-target class="md-primary">
+              <md-icon class="md-morph-initial">person</md-icon>
+              <md-icon class="md-morph-final">close</md-icon>
+            </md-speed-dial-target>
+
+            <md-speed-dial-content>
+              <md-button class="md-icon-button">
+              <md-icon>directions</md-icon>
+            </md-button>
+
+            <md-button class="md-icon-button">
+              <md-icon>streetview</md-icon>
+            </md-button>
+          </md-speed-dial-content>
+        </md-speed-dial>
+      </md-app-content>
+    </md-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: () => ({
+    menuVisible: true
+  }),
+  methods: {
+    toggleMenu () {
+      this.menuVisible = !this.menuVisible
+    },
+    mobileMenu () {
+      var width = window.innerWidth
+      if (width < 600) {
+        this.menuVisible = false
+      }
+    },
+    registerCompo () {
+      alert('register')
+    },
+    loginCompo () {
+      alert('login')
+    }
+  },
+  beforeMount: function () {
+    this.mobileMenu()
+  }
+}
+</script>
+
+<style lang="scss">
+  .md-app {
+    font-family: 'Quicksand', sans-serif;
+    min-height: 700px;  
+    overflow: auto;
+    border: 2px solid rgba(#000, .12);
+  }
+  .appContent{
+    padding: 0px; 
+    margin: 0px;
+     height: 600px;
+  }
+   // Demo purposes only
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+  }
+  .pointer{
+    cursor: pointer;
+  }
+
+  .logoColor{
+    color: #fff;
+  }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="content">
+    <div v-if="spinner">
+      <center>
+        <md-progress-spinner class="md-primary centerSpinner" :md-stroke="3" md-mode="indeterminate"></md-progress-spinner>
+      </center>
+    </div>
+    <div v-else class="animated fadeInUp">
+      <div class="jumbo">
+        <img src="../assets/img/zhen-hu-340739.jpg" alt="" class="jumboImg">
+        <div class="welcomeNote">
+          <div class="welcomeTitle">
+            <span class="md-display-1">Locate the nearest Health Care Center</span>
+          </div>
+          <div class="welcomeSub">
+            <span class="md-subheading">Eaisest way to locate a Health Care Center in less than a minute! </span>
+          </div>
+          <md-card class="jumboCard">
+            <md-card-header>
+              <md-card-header-text>
+                <div class="md-title">Lets Get</div>
+                <div class="md-title">Started</div>
+              </md-card-header-text>
+
+            <md-card-media md-medium>
+              <md-icon class="md-size-5x animated infinite pulse">my_location</md-icon>
+              </md-card-media>
+            </md-card-header>
+
+            <md-card-actions>
+              <md-button class="md-raised md-primary">User Guide</md-button>
+              <md-button class="md-raised md-accent">Ambluance</md-button>
+              <md-button class="md-raised md-primary">Emergency?</md-button>
+            </md-card-actions>
+          </md-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data: () => ({
+    msg: 'Welcome to MedFind Home Page!',
+    spinner: true
+  }),
+  methods: {
+    spinnerShow (spinner) {
+      setTimeout((spinner) => {
+        this.spinner = false
+      }, 3000)
+    }
+  },
+  mounted: function () {
+    this.spinnerShow()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .centerSpinner{
+    margin: 250px 0 250px 0;
+  }
+  .jumbo{
+    position: relative;
+    height: 633px;
+  }
+  .jumboImg{
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 50;
+    left: 0;
+    -o-object-fit: cover;
+    object-fit: cover;
+    z-index: -1;
+    -webkit-filter: brightness(0.7);
+    filter: brightness(0.7);
+    object-position: 0 0;
+
+    // &:before {
+    // content: '';
+	  // position: absolute;
+	  // top: 0;
+	  // right: 0;
+	  // bottom: 0;
+	  // left: 0;
+	  // background-image: linear-gradient(to bottom right,#002f4b,#dc4225);
+	  // opacity: .6; 
+    // }
+  }
+
+  .welcomeNote{
+    padding: 40px;
+    text-align: center;
+    color: #fff;
+    font-weight: bold;
+  }
+  .jumboCard{
+    padding-top: 20px;
+    width: 40%;
+    margin: 0px auto;
+    background-image: linear-gradient(to bottom right,#ffffff,#0d9bee);
+  }
+
+  @media only screen and (max-width: 600px) {
+  .jumboCard{
+    width: 100%;
+    }
+  }
+  @media only screen and (min-width: 600px) and (max-width: 992px) {
+  .jumboCard{
+    width: 50%;
+    }
+  }
+</style>
